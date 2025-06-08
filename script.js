088195e1a605a3341256101cf97ed63d966c770d
nnasef1 <nnasef@gmail.com>
2025-06-07 23:17:03 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+// script.js
+document.querySelector('.nav-toggle').addEventListener('click', () => {
+  document.querySelector('.site-nav').classList.toggle('open');
+});
+
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
